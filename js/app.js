9424d3eaad2a092fb2b0bcde642c54328b5ca4f8
raind530 <77490475+raind530@users.noreply.github.com>
2021-03-06 15:53:31 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+const { ref, onMounted, watch, computed, reactive } = Vue
+const { useDeviceOrientation, useMouse } = VueUse
 
+const app = Vue.createApp({
+  setup(context) {
+
+    const restart = (retry) => {
+      game.value = 1
+    }
+    
+    const roadimg = computed(() => {
+          return game.value < 2? 'url(./assets/road.gif)' : 'url(./assets/road.jpg)'
+        })
+   
+  }
+}
